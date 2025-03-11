c21797d5e303d194f6f5e5bcbe5cc77328ae4611
Your Name <you@example.com>
2025-03-11 16:45:52 +0300

    add assignment

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Write your solution in this file!
+// Define the employee object
+const employee = {
+    name: "Sam",
+    streetAddress: "11 Broadway"
+  };
+  
+  // 1. Non-destructive update
+  function updateEmployeeWithKeyAndValue(employee, key, value) {
+    return { ...employee, [key]: value };
+  }
+  
+  // 2. Destructive update
+  function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value;
+    return employee;
+  }
+  
+  // 3. Non-destructive delete
+  function deleteFromEmployeeByKey(employee, key) {
+    const newEmployee = { ...employee };
+    delete newEmployee[key];
+    return newEmployee;
+  }
+  
+  // 4. Destructive delete
+  function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee[key];
+    return employee;
+  }
+  
+
+  
